cb076cc0a0e941fc3b159701db8f11668e9895b0
Ayon Lee <i@hyurl.com>
2018-11-18 09:30:22 +0800

    fix a type bug

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,24 @@ export interface Interceptable<T> {
     after(listener: (...args) => false | void | any[]): this;
 }
 
+export interface InterceptableAsync<T> {
+    /** Returns the original function. */
+    readonly target: T;
+    /** Adds listeners that will be called before invoking the function. */
+    before(listener: (...args) => false | void | any[] | Promise<false | void | any[]>): this;
+    /** Adds listeners that will be called after invoking the function. */
+    after(listener: (...args) => false | void | any[] | Promise<false | void | any[]>): this;
+}
+
 export interface InterceptableDecorator<T> {
+    (proto: any, prop: string, desc?: PropertyDescriptor): void;
+    /** Adds listeners that will be called before invoking the method. */
+    before(listener: (this: T, ...args) => false | void | any[]): this;
+    /** Adds listeners that will be called after invoking the method. */
+    after(listener: (this: T, ...args) => false | void | any[]): this;
+}
+
+export interface InterceptableAsyncDecorator<T> {
     (proto: any, prop: string, desc?: PropertyDescriptor): void;
     /** Adds listeners that will be called before invoking the method. */
     before(listener: (this: T, ...args) => false | void | any[] | Promise<false | void | any[]>): this;
@@ -20,8 +37,8 @@ export function intercept<T>(fn: T): T & Interceptable<T>;
 /** Gets a chained interceptable decorator. */
 export function intercept<T = any>(): InterceptableDecorator<T>;
 /** Intercepts the given fuction and run actions asynchornously and sequencially. */
-export function interceptAsync<T extends (...args) => any>(fn: T): (ReturnType<T> extends Promise<any> ? T : (...args) => Promise<ReturnType<T>>) & Interceptable<T>;
+export function interceptAsync<T extends (...args) => any>(fn: T): (ReturnType<T> extends Promise<any> ? T : (...args) => Promise<ReturnType<T>>) & InterceptableAsync<T>;
 /** Gets a chained interceptable decorator and run actions asynchornously and sequencially. */
-export function interceptAsync<T = any>(): InterceptableDecorator<T>;
+export function interceptAsync<T = any>(): InterceptableAsyncDecorator<T>;
 
 export default intercept;
